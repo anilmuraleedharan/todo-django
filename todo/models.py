920b3fe8bd5d1fc7fbca86824b9d63c9f3592b5d
kannananil <56071561+kannananil@users.noreply.github.com>
2023-09-24 16:12:38 +0530

    [ Anil ] Altered Task model field status to a boolean value

--- todo/models.py
+++ todo/models.py
@@ -2,8 +2,6 @@ from enum import Enum
 
 from django.db import models
 
-task_status_choices = [('pending', 'Pending'), ('in_progress', 'In Progress'), ('completed', 'Completed')]
-
 
 class Todo(models.Model):
     id = models.AutoField(primary_key=True)
@@ -19,7 +17,7 @@ class Task(models.Model):
     todo_id = models.ForeignKey(Todo, on_delete=models.CASCADE)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=255, choices=task_status_choices)
+    status = models.BooleanField(default=False)
 
     def __str__(self):
         return self.description
